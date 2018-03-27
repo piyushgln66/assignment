d70ac3165e5bbeca33a1cac73764b8aee52801a6
Piyush Golani <piyush_g@worksap.co.jp>
2018-03-27 18:48:36 +0800

    added validations for arguments passed in the API call

--- src/main/java/com/sp/group/controller/FriendsController.java
+++ src/main/java/com/sp/group/controller/FriendsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import com.sp.group.exception.CustomException;
 import com.sp.group.request.EmailRequest;
 import com.sp.group.request.FriendsRequest;
 import com.sp.group.request.SubscribeBlockRequest;
@@ -17,6 +18,8 @@ import com.sp.group.request.UpdateRequest;
 import com.sp.group.response.CustomResponse;
 import com.sp.group.response.Views;
 import com.sp.group.service.FriendsService;
+import com.sp.group.util.EmailValidatorUtil;
+import com.sp.group.util.UpdateValidatorUtil;
 
 @RestController
 public class FriendsController {
@@ -41,10 +44,14 @@ public class FriendsController {
      * as friends
      * @param friends This parameter contains 2 email addresses
      * @return CustomResponse
+     * @throws CustomException
      */
     @JsonView(Views.Connect.class)
 	@RequestMapping(value="/connect", method=RequestMethod.POST)
-	public CustomResponse connect(@RequestBody FriendsRequest friends) {
+	public CustomResponse connect(@RequestBody FriendsRequest friends) throws CustomException {
+    	if(friends.getFriends().size()!=2) throw new CustomException("Please provide 2 email addresses");
+    	EmailValidatorUtil.validate(friends.getFriends());
+    	if(friends.getFriends().get(0).equals(friends.getFriends().get(1))) throw new CustomException("Both email addresses cannot be same");
 		friendsService.connect(friends.getFriends().get(0), friends.getFriends().get(1));
 		return new CustomResponse();
 	}
@@ -54,10 +61,12 @@ public class FriendsController {
      * friends of a person
      * @param emailRequest This parameter contains an email address
      * @return CustomResponse
+     * @throws CustomException
      */
     @JsonView(Views.FriendList.class)
 	@RequestMapping(value="/getFriendsList", method=RequestMethod.POST)
-	public CustomResponse getFriendsList(@RequestBody EmailRequest emailRequest) {
+	public CustomResponse getFriendsList(@RequestBody EmailRequest emailRequest) throws CustomException {
+    	EmailValidatorUtil.validate(emailRequest.getEmail());
 		List<String> friendsList = friendsService.getFriendsList(emailRequest.getEmail());
 		return new CustomResponse(true, friendsList, friendsList.size());
 	}
@@ -67,10 +76,14 @@ public class FriendsController {
      * friends of two people
      * @param friends This parameter contains 2 email addresses
      * @return CustomResponse
+     * @throws CustomException
      */
     @JsonView(Views.FriendList.class)
 	@RequestMapping(value="/getMutualFriends", method=RequestMethod.POST)
-	public CustomResponse getMutualFriends(@RequestBody FriendsRequest friends) {
+	public CustomResponse getMutualFriends(@RequestBody FriendsRequest friends) throws CustomException {
+    	if(friends.getFriends().size()!=2) throw new CustomException("Please provide 2 email addresses");
+    	EmailValidatorUtil.validate(friends.getFriends());
+    	if(friends.getFriends().get(0).equals(friends.getFriends().get(1))) throw new CustomException("Both email addresses cannot be same");
 		List<String> friendsList = friendsService.getMutualFriends(friends.getFriends().get(0),
 				friends.getFriends().get(1));
 		return new CustomResponse(true, friendsList, friendsList.size());
@@ -81,10 +94,14 @@ public class FriendsController {
      * updates from a person
      * @param subscribeRequest This parameter contains 2 email addresses, the requestor and the target
      * @return CustomResponse
+     * @throws CustomException
      */
     @JsonView(Views.Connect.class)
 	@RequestMapping(value="/subscribe", method=RequestMethod.POST)
-	public CustomResponse subscribe(@RequestBody SubscribeBlockRequest subscribeRequest) {
+	public CustomResponse subscribe(@RequestBody SubscribeBlockRequest subscribeRequest) throws CustomException {
+    	EmailValidatorUtil.validate(subscribeRequest.getRequestor());
+    	EmailValidatorUtil.validate(subscribeRequest.getTarget());
+    	if(subscribeRequest.getRequestor().equals(subscribeRequest.getTarget())) throw new CustomException("Both email addresses cannot be same");
 		friendsService.subscribe(subscribeRequest.getRequestor(), subscribeRequest.getTarget());
 		return new CustomResponse();
 	}
@@ -94,10 +111,14 @@ public class FriendsController {
      * updates from a person
      * @param blockRequest This parameter contains 2 email addresses, the requestor and the target
      * @return CustomResponse
+     * @throws CustomException
      */
     @JsonView(Views.Connect.class)
 	@RequestMapping(value="/block", method=RequestMethod.POST)
-	public CustomResponse block(@RequestBody SubscribeBlockRequest blockRequest) {
+	public CustomResponse block(@RequestBody SubscribeBlockRequest blockRequest) throws CustomException {
+    	EmailValidatorUtil.validate(blockRequest.getRequestor());
+    	EmailValidatorUtil.validate(blockRequest.getTarget());
+    	if(blockRequest.getRequestor().equals(blockRequest.getTarget())) throw new CustomException("Both email addresses cannot be same");
 		friendsService.block(blockRequest.getRequestor(), blockRequest.getTarget());
 		return new CustomResponse();
 	}
@@ -107,11 +128,15 @@ public class FriendsController {
      * people who will receive the updates from a given person
      * @param updateRequest This parameter contains the sender email and the text
      * @return CustomResponse
+     * @throws CustomException
      */
     @JsonView(Views.RecipientList.class)
 	@RequestMapping(value="/send", method=RequestMethod.POST)
-	public CustomResponse send(@RequestBody UpdateRequest updateRequest) {
+	public CustomResponse send(@RequestBody UpdateRequest updateRequest) throws CustomException {
+    	EmailValidatorUtil.validate(updateRequest.getSender());
+    	UpdateValidatorUtil.validate(updateRequest.getText());
     	List<String> recipientsList = friendsService.getUpdateRecipients(updateRequest.getSender(), updateRequest.getText());
+    	recipientsList = EmailValidatorUtil.removeDuplicate(recipientsList);
 		return new CustomResponse(true, recipientsList);
 	}
 }
