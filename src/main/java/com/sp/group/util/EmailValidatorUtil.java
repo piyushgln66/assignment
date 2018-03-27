c9ddf70912e0cd98f7af87218c6f8cca91b50350
Piyush Golani <piyush_g@worksap.co.jp>
2018-03-27 13:19:55 +0800

    added a utility class to validate an email address

---
+++ src/main/java/com/sp/group/util/EmailValidatorUtil.java
@@ -0,0 +1,18 @@
+package com.sp.group.util;
+
+import com.sp.group.exception.CustomException;
+
+public class EmailValidatorUtil {
+	
+	private final static String PATTERN_STRING = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+	
+	public static void validate(String email) throws CustomException  {
+		if(email.isEmpty()) {
+			throw new CustomException("Email address is empty");
+		}
+		if(!email.matches(PATTERN_STRING)) {
+			throw new CustomException("Please provide a valid email address");
+		}
+	}
+
+}
